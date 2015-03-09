a3f6b86922abfd24c78d3ba2acb7e859a7e1d0b4
Sourec <tothemyers@gmail.com>
2015-03-09 13:46:44 -0400

    Added documentation for OctocanumDrive

--- src/drivebase/OctocanumDrive.h
+++ src/drivebase/OctocanumDrive.h
@@ -12,10 +12,10 @@ namespace ADBLib
 		enum driveMode {mecanum, traction};
 
 		OctocanumDrive();
-		void drive(float x = 0.0, float y = 0.0, float r = 0.0);
-		void switchMode(driveMode newMode);
-		void setPneumatic(SimplePneumatic* newPneumatic, MotorPos pos);
-		driveMode getMode();
+		void drive(float x = 0.0, float y = 0.0, float r = 0.0); //!< Drive with either traction or mecanum. Automatically extends or retracts pneumatics.
+		void switchMode(driveMode newMode); //!< Allows switching from one mode to another.
+		void setPneumatic(SimplePneumatic* newPneumatic, MotorPos pos); //!< Sets the pneumatic assigned to a single wheel. Based off of wheel positions.
+		driveMode getMode(); //!< Returns the mode that the octocanum drive is currently in.
 	protected:
 		SimplePneumatic* solenoids[4];
 		driveMode mode;
