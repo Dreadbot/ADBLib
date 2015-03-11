8b0c5cf163c1b8c009539a448a854ff3e6246dd5
Sourec <tothemyers@gmail.com>
2015-03-11 12:44:30 -0400

    Added documentation for MotorGroup and PneumaticGroup.

--- src/output/MotorGroup.h
+++ src/output/MotorGroup.h
@@ -9,15 +9,15 @@ using std::string;
 
 namespace ADBLib
 {
-	class MotorGroup
+	class MotorGroup //!< Simple motor grouping class. Allows control over multiple motors without additional code.
 	{
 	public:
 		MotorGroup();
-		void set(float value);
-		void addMotor(SimpleMotor* newMotor);
+		void set(float value); //!< Pass the given value to all motors in this motor group.
+		void addMotor(SimpleMotor* newMotor); //!< Add a new motor to the motor group.
 
-		void setName(string newName);
-		string getName();
+		void setName(string newName); //!< Sets the name of this motor group.
+		string getName(); //!< Gets the name of this motor group.
 	protected:
 		vector<SimpleMotor*> motors;
 		string name;
--- src/output/PneumaticGroup.h
+++ src/output/PneumaticGroup.h
@@ -9,15 +9,15 @@ using std::vector;
 
 namespace ADBLib
 {
-	class PneumaticGroup
+	class PneumaticGroup //!< Simple pneumatic grouping class. Allows control over multiple pneumatics without additional code.
 	{
 	public:
 		PneumaticGroup();
-		void set(float value);
-		void addPneumatic(SimplePneumatic* newPneumatic);
+		void set(float value); //!< Pass the given value to all the pneumatics in this group.
+		void addPneumatic(SimplePneumatic* newPneumatic); //!< Add a new pneumatic to this pneumatic group.
 
-		void setName(string newName);
-		string getName();
+		void setName(string newName); //!< Sets the name of this pneumatic group.
+		string getName(); //!< Gets the name of this pneumatic group.
 	protected:
 		vector<SimplePneumatic*> pneumatics;
 		string name;
