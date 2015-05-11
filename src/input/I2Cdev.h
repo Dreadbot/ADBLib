093ae2f933c6fd166629b59567790b77606b4072
Sourec <tothemyers@gmail.com>
2015-05-11 19:11:38 -0400

    Added MadgwickAHRS files, fixed I2Cdev indentation
    Madgwick's filter is a really awesome filter for filtering out noise (in a sense) and generally just for correcting attitude/acceleration measurements.

--- src/input/I2Cdev.h
+++ src/input/I2Cdev.h
@@ -18,37 +18,37 @@
 
 namespace ADBLib
 {
-template <typename T>
-void assert(T toassert, std::string msg)
-{
-    if (toassert)
-        SmartDashboard::PutBoolean("Assert::" + msg, true);
-    else
-        SmartDashboard::PutBoolean("Assert::" + msg, false);
-}
+	template <typename T>
+	void assert(T toassert, std::string msg)
+	{
+		if (toassert)
+			SmartDashboard::PutBoolean("Assert::" + msg, true);
+		else
+			SmartDashboard::PutBoolean("Assert::" + msg, false);
+	}
 
-class I2Cdev
-{
-public:
-    I2Cdev();
-    static void initialize();
+	class I2Cdev
+	{
+	public:
+		I2Cdev();
+		static void initialize();
 
-    static int8_t readBit(uint8_t _devAddr, uint8_t regAddr, uint8_t bitNum, uint8_t *data);
-    static int8_t readBits(uint8_t _devAddr, uint8_t regAddr, uint8_t bitStart, uint8_t length, uint8_t *data);
-    static int8_t readByte(uint8_t _devAddr, uint8_t regAddr, uint8_t *data);
-    // readBytes implements all core functionality
-    static int8_t readBytes(uint8_t _devAddr, uint8_t regAddr, uint8_t length, uint8_t *data);
+		static int8_t readBit(uint8_t _devAddr, uint8_t regAddr, uint8_t bitNum, uint8_t *data);
+		static int8_t readBits(uint8_t _devAddr, uint8_t regAddr, uint8_t bitStart, uint8_t length, uint8_t *data);
+		static int8_t readByte(uint8_t _devAddr, uint8_t regAddr, uint8_t *data);
+		// readBytes implements all core functionality
+		static int8_t readBytes(uint8_t _devAddr, uint8_t regAddr, uint8_t length, uint8_t *data);
 
-    static bool writeBit(uint8_t _devAddr, uint8_t regAddr, uint8_t bitNum, uint8_t data);
-    static bool writeBits(uint8_t _devAddr, uint8_t regAddr, uint8_t bitStart, uint8_t length, uint8_t data);
-    static bool writeByte(uint8_t _devAddr, uint8_t regAddr, uint8_t data);
-    // writeBytes implements all core write functionality
-    static bool writeBytes(uint8_t _devAddr, uint8_t regAddr, uint8_t length, uint8_t *data);
+		static bool writeBit(uint8_t _devAddr, uint8_t regAddr, uint8_t bitNum, uint8_t data);
+		static bool writeBits(uint8_t _devAddr, uint8_t regAddr, uint8_t bitStart, uint8_t length, uint8_t data);
+		static bool writeByte(uint8_t _devAddr, uint8_t regAddr, uint8_t data);
+		// writeBytes implements all core write functionality
+		static bool writeBytes(uint8_t _devAddr, uint8_t regAddr, uint8_t length, uint8_t *data);
 
-    const static uint8_t devAddr = MPU9150_I2C_ADR;
-    static I2C* device;
+		const static uint8_t devAddr = MPU9150_I2C_ADR;
+		static I2C* device;
 
-private:
-    const static uint16_t readTimeout = 1000;
-};
+	private:
+		const static uint16_t readTimeout = 1000;
+	};
 }
