625184147f2e93309f93f50cda566fb9a55be8b2
Sourec <tothemyers@gmail.com>
2015-09-18 17:47:28 -0400

    Fixed MecanumDrive & MecanumDrive constructors

--- src/drivebase/TractionDrive.h
+++ src/drivebase/TractionDrive.h
@@ -10,6 +10,10 @@ namespace ADBLib
 	class TractionDrive : public Drivebase
 	{
 	public:
+		TractionDrive(SpeedController* mFrontLeft,
+					SpeedController* mFrontRight,
+					SpeedController* mBackRight,
+					SpeedController* mBackLeft) : Drivebase(mFrontLeft, mFrontRight, mBackRight, mBackLeft) {}
 		void drive(float x = 0.0, float y = 0.0, float r = 0.0);
 	};
 }
