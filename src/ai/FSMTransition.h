0efa7eda72c851959fa7da2bd084cc9aec310a77
Sourec <tothemyers@gmail.com>
2016-02-12 18:02:04 -0500

    Fix END_STATE_TABLE define causing bool -> ptr conversion error

--- src/ai/FSMTransition.h
+++ src/ai/FSMTransition.h
@@ -2,15 +2,14 @@
 
 #include "FSMState.h"
 
-#define END_STATE_TABLE {nullptr, 0, false} //!< Append this to the end of your state tables; the init function will recognize it and stop searching the table for states.
+#define END_STATE_TABLE {nullptr, 0, nullptr} //!< Append this to the end of your state tables; the init function will recognize it and stop searching the table for states.
 
 namespace ADBLib
 {
-	class FSMTransition
+	struct FSMTransition
 	{
-	public:
 		FSMState* currentState;	//!< The current state.
-		int input;				//!< If this input is recieved and the FSM state is the currentState, transition.
+		int input;				//!< If this input is received and the FSM state is the currentState, transition.
 		FSMState* nextState;	//!< The next state to transition to.
 	};
 }
